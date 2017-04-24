45adce9452d721ad1fd45fe7c86491ac8cf268d8
psanmuk <panya.spcom@gmail.com>
2017-04-24 12:39:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,9 @@
 <?php
 $access_token = 'N/gJOEKBTBlgJ9VVXWakLrWgGGB31Rpn1FJ4gWqCzW4p5/ZCfCVl/7wx0IUlEu5mdza0AoHnL/sSjruIu9d6fyRXo008ty2kvqKGW60hvfa8oPtBXywlxLinfI/bGZWjQQ3GauItv80x93Pehic7hgdB04t89/1O/w1cDnyilFU=';
 
+$proxy = 'http://fixie:7L2IiwTyTAtekGS@velodrome.usefixie.com:80';
+$proxyauth = 'panya.spcom@gmail.com:Panya@97';
+
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -31,12 +34,18 @@ if (!is_null($events['events'])) {
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
+			
+			
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			
 			$result = curl_exec($ch);
 			curl_close($ch);
 
