c4d59022a862c14cfb8710242d1a0ddb77b89afe
psanmuk <panya.spcom@gmail.com>
2017-04-24 15:51:29 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -50,6 +50,16 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
+			
+//$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
+//$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->replyMessage($replyToken, $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();			
+			
+			
 		}
 	}
 }
