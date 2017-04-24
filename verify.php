d20aa7f48dfba0121ded5d007e8fe439a92a6019
psanmuk <panya.spcom@gmail.com>
2017-04-24 12:41:51 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -5,10 +5,16 @@ $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
+$proxy = 'http://fixie:7L2IiwTyTAtekGS@velodrome.usefixie.com:80';
+$proxyauth = 'panya.spcom@gmail.com:Panya@97';
+
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
